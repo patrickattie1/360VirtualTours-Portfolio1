2ae3122a51fbd70ed8a9d92d7a3f88a509705490
Patrick ATTIE <patrick.attie@esih.edu>
2019-08-15 10:57:38 -0500

    Commit # 1 - August 15th, 2019 - Samsung Gear VR version using reticle for navigation.

---
+++ Assets/Datafiles/Scripts/CanvasFaceCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasFaceCamera : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Get direction = (pos of canvas - pos of camera)
+		Vector3 direction = transform.position - Camera.main.transform.position;
+		//Set forward direction of the canvas
+		transform.forward = direction;
+	}
+}
---
+++ Assets/Datafiles/Scripts/DestinationPinController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+using UnityEngine.SceneManagement;
+
+[RequireComponent (typeof(VRInteractiveItem))]
+public class DestinationPinController : MonoBehaviour 
+{
+	//Name of the destination Scene
+	public string sceneName;
+
+	//VR Interactive object component
+	VRInteractiveItem vrInteractive;
+
+	void Awake()
+	{
+		vrInteractive = GetComponent<VRInteractiveItem> ();
+	}
+
+	void ChangeScene()
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+
+	//Called when gameObject becomes enabled
+	void OnEnable()
+	{
+		vrInteractive.OnClick += ChangeScene;
+	}
+
+	//Called when gameObjects becomes disabled
+	void OnDisable()
+	{
+		vrInteractive.OnClick -= ChangeScene;
+	}
+}
